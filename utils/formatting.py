abf1413b6ecd59e60e17e7af0671d3c3ca2f1f9b
╔ shceo ╝ 大 市 文 <104291367+shceo@users.noreply.github.com>
2025-07-20 20:10:09 +0500

    j

--- utils/formatting.py
+++ utils/formatting.py
@@ -6,13 +6,13 @@ def short_number(value: int | float) -> str:
     if value >= 1_000_000:
         # e.g. 1500000 -> 1.5кк
         if value % 1_000_000 == 0:
-            return f"{value // 1_000_000}кк"
+            return f"{value // 1_000_000}k"
         else:
-            return f"{value / 1_000_000:.1f}кк"
+            return f"{value / 1_000_000:.1f}k"
     elif value >= 1000:
         if value % 1000 == 0:
-            return f"{value // 1000}к"
+            return f"{value // 1000}"
         else:
-            return f"{value / 1000:.1f}к"
+            return f"{value / 1000:.1f}k"
     else:
         return str(value)
